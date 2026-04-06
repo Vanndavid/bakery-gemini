e2b500832d6da2c7e7a6e920209fa6486f5c6450
Vanndavid Teng <44202287+Vanndavid@users.noreply.github.com>
2026-04-06 10:32:49 +1000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { ArrowRight, Star } from 'lucide-react';
 import { Menu } from './Menu';
